35207bd3b6adaa27474ce7d76894b3e7d4d76fa2
saphron <saphron.scul@gmail.com>
2015-10-12 17:43:30 -0400

    starting on javascript/buttons

---
+++ tttgame.js
@@ -0,0 +1,40 @@
+$("#div1").click(function() {
+    alert("I was clicked.");
+});
+$("#div2").click(function() {
+    alert("I was clicked.");
+});
+$("#div3").click(function() {
+    alert("I was clicked.");
+});
+$("#div4").click(function() {
+    alert("I was clicked.");
+});
+$("#div5").click(function() {
+    alert("I was clicked.");
+});
+$("#div6").click(function() {
+    alert("I was clicked.");
+});
+$("#div7").click(function() {
+    alert("I was clicked.");
+});
+$("#div8").click(function() {
+    alert("I was clicked.");
+});
+$("#div9").click(function() {
+    alert("I was clicked.");
+});
+
+
+// <div class="ui-dialog-buttonpane ui-widget-content ui-helper-clearfix">
+//    <div class="ui-dialog-buttonset">
+//       <button class="otherButtonClass ui-state-hover ui-state-focus" type="button" role="button" aria-disabled="false">
+//       <button class="otherButtonClass" type="button" role="button" aria-disabled="false" style="display: none;">
+//       <button class="cancelButtonClass" type="button" role="button" aria-disabled="false">
+//    </div>
+// </div>
+
+// var buttons = document.querySelectorAll('.ui-dialog-buttonset button');
+//     buttons[0].setAttribute('aria-disabled', true);
+//     buttons[1].setAttribute('aria-disabled', true);
