d6d25d5fd6f1c003b0d75377730bdea78e49447f
saphron <saphron.scul@gmail.com>
2015-10-14 00:14:46 -0400

    tests

---
+++ test.js
@@ -0,0 +1,11 @@
+// result = window.prompt(message, default);
+
+var name = window.prompt("What's your name?", 'Daleks');
+  if (player_x) {
+    window.alert("You are the first player.  Your team is the Daleks!");
+  }
+
+var name = window.prompt("What's your name?", 'Tardis');
+  if (player_o) {
+    window.alert("You are the second player.  Your team is the Doctor!");
+  }
