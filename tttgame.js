8ffcd5cf9743008ed37b97b5d2aad56c81b266af
saphron <saphron.scul@gmail.com>
2015-10-14 00:13:52 -0400

    javascript is working, rearranged the files

--- tttgame.js
+++ tttgame.js
@@ -1,57 +1,111 @@
-
-var playerOne = {
+var player_x = {
   name: null,
   icon: null,
 };
 
-var playerTwo = {
+var player_o = {
   name: null,
   icon: null,
 };
 
 var gameBoard = {};
 
-// players each enter names, game remembers player names
-playerOne.name = prompt('What is your name?');
-playerTwo.name = prompt('What is your name?');
 
-document.getElementById('1name').textContent = playerOne.name;
-document.getElementById('2name').textContent = playerTwo.name;
-
+// player 1 enter name, game remembers player name
+player_x.name = window.prompt('What is your name?', 'the Daleks');
+window.alert(player_x.name + ", you are the first player.  Your team is the Daleks!");
 // player 1 assigned icon, game remembers that icon to represent player 1
-// player 2 assigned different icon, game remembers that icon to represent player 2
-var tardisSnippet = '<img src="tardis.png" height="100px">';
+document.getElementById('Xname').textContent = player_x.name;
 
-var dalekSnippet = '<img src="dalek.png" height="100px">';
 
-// In order to take turns there are four things it needs to do:
-// 1. Know whose turn it is
+// player 2 enters name, game remembers player name
+player_o.name = window.prompt('What is your name?', 'the Doctor');
+window.alert(player_o.name + ", you are the second player.  Your team is the Doctor!");
+// player 2 assigned second icon, game remembers that icon to represent player 2
+document.getElementById('Oname').textContent = player_o.name;
+
+
+function getWinner() {
+  if (
+    (gameBoard['div1'] === 'dalek' && gameBoard['div2'] === 'dalek' && gameBoard['div3'] === 'dalek') ||
+    (gameBoard['div4'] === 'dalek' && gameBoard['div5'] === 'dalek' && gameBoard['div6'] === 'dalek') ||
+    (gameBoard['div7'] === 'dalek' && gameBoard['div8'] === 'dalek' && gameBoard['div9'] === 'dalek') ||
+    (gameBoard['div1'] === 'dalek' && gameBoard['div4'] === 'dalek' && gameBoard['div7'] === 'dalek') ||
+    (gameBoard['div2'] === 'dalek' && gameBoard['div5'] === 'dalek' && gameBoard['div8'] === 'dalek') ||
+    (gameBoard['div3'] === 'dalek' && gameBoard['div6'] === 'dalek' && gameBoard['div9'] === 'dalek') ||
+    (gameBoard['div1'] === 'dalek' && gameBoard['div5'] === 'dalek' && gameBoard['div9'] === 'dalek') ||
+    (gameBoard['div3'] === 'dalek' && gameBoard['div5'] === 'dalek' && gameBoard['div7'] === 'dalek') ) {
+    return player_x.name;
+  } else if (
+    (gameBoard['div1'] === 'tardis' && gameBoard['div2'] === 'tardis' && gameBoard['div3'] === 'tardis') ||
+    (gameBoard['div4'] === 'tardis' && gameBoard['div5'] === 'tardis' && gameBoard['div6'] === 'tardis') ||
+    (gameBoard['div7'] === 'tardis' && gameBoard['div8'] === 'tardis' && gameBoard['div9'] === 'tardis') ||
+    (gameBoard['div1'] === 'tardis' && gameBoard['div4'] === 'tardis' && gameBoard['div7'] === 'tardis') ||
+    (gameBoard['div2'] === 'tardis' && gameBoard['div5'] === 'tardis' && gameBoard['div8'] === 'tardis') ||
+    (gameBoard['div3'] === 'tardis' && gameBoard['div6'] === 'tardis' && gameBoard['div9'] === 'tardis') ||
+    (gameBoard['div1'] === 'tardis' && gameBoard['div5'] === 'tardis' && gameBoard['div9'] === 'tardis') ||
+    (gameBoard['div3'] === 'tardis' && gameBoard['div5'] === 'tardis' && gameBoard['div7'] === 'tardis') ) {
+    return player_o.name;
+  } else {
+    return null;
+  }
+};
+
+
 var whoseTurn;
 whoseTurn = 1;
 
-// 2. Be able to switch whose turn it is
 function switchTurn(){
-  if (whoseTurn == 1){
+  if (whoseTurn === 1){
     whoseTurn = 2;
-  } else if (whoseTurn == 2){
+  } else if (whoseTurn === 2){
     whoseTurn = 1;
   }
 };
 
+
+var dalekSnippet = '<img src="assets/dalek.png" height="100px">';
+var tardisSnippet = '<img src="assets/tardis.png" height="100px">';
+
+
+// To take turns, need to:
+// 1. Know whose turn it is
+// 2. Be able to switch whose turn it is
 // 3. Know to switch whose turn it is whenever someone clicks
 // 4. Know to put the right icon in the right square when someone clicks
+// 5. Know to end game when 3 same icons form a row
+// 6. Know to annouce game finished at end game
+// 7. Know to annouce winner
+// 8. Know to ask "play again?"
+
+
 function setDivClickFunction(idToUse){
   var div = document.getElementById(idToUse);
   div.onclick = function() {
-    if (gameBoard[idToUse] != 'played'){
-      if (whoseTurn == 1){
-        document.getElementById(idToUse).innerHTML = tardisSnippet;
-      } else if (whoseTurn == 2){
+    if (gameBoard[idToUse] !== 'dalek' && gameBoard[idToUse] !== 'tardis') {
+      if (whoseTurn === 1) {
         document.getElementById(idToUse).innerHTML = dalekSnippet;
+        gameBoard[idToUse] = 'dalek';
       }
-      switchTurn();
+      if (whoseTurn === 2) {
+        document.getElementById(idToUse).innerHTML = tardisSnippet;
+        gameBoard[idToUse] = 'tardis';
+      }
+
+
+      var winner = getWinner();
+      if (winner) {
+        whoseTurn = 3;
+        window.alert("Game Over! " + winner + " has won!");
+        if (window.confirm("Do you want to play again?")) {
+          window.open("index.html", "Reset the board!");
+        }
+      } else {
+        switchTurn();
+      }
+
+
     }
-    gameBoard[idToUse] = 'played';
     console.log(gameBoard);
   };
 };
@@ -65,11 +119,3 @@ setDivClickFunction('div6');
 setDivClickFunction('div7');
 setDivClickFunction('div8');
 setDivClickFunction('div9');
-
-// 5. Know to end game when 3 same icons form a row
-
-// 6. Know to annouce game finished at end game
-
-// 7. Know to annouce score and winner
-
-// 8. Know to ask "play again?"
